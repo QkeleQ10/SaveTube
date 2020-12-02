2148beeb0021d01cf3d5c7b268a9cbfe52015735
QkeleQ10 <quintenra@gmail.com>
2020-12-02 22:41:39 +0100

    CSS and server improvements - not finished yet

--- Server/index.js
+++ Server/index.js
@@ -6,15 +6,21 @@ const app = express()
 app.use(cors())
 
 app.listen(process.env.PORT || 4000, () => {
-    console.log("Server online at port" + process.env.PORT || "4000")
+    console.log("Server online at port " + process.env.PORT || "4000")
 })
 
 app.get("/download", async (req, res) => {
-    var URL = req.query.URL
-    var ext = req.query.ext
-    var filter
-    if (ext === "mp3") filter = "audioonly"
-    else filter = "videoandaudio"
-    res.header("Content-Disposition", `attachment; filename="download.${ext}"`)
-    ytdl(URL, { format: ext, filter: filter, }).pipe(res)
+    try {
+        var URL = req.query.URL
+        var ext = req.query.ext
+        var filter
+        if (ext === "mp3") filter = "audioonly"
+        else filter = "videoandaudio"
+        res.header("Content-Disposition", `attachment; filename="download.${ext}"`)
+        ytdl(URL, { format: ext, filter: filter, }).pipe(res)
+    } catch (err) { throw err }
+})
+
+app.get("/", function (req, res) {
+    throw new Error('BROKEN')
 })
--- script.js
+++ script.js
@@ -3,10 +3,25 @@ var URLinput = document.getElementById('URL-input')
 var ext = document.getElementById('extension')
 
 convertBtn.addEventListener('click', () => {
-    console.log(`URL: ${URLinput.value}`)
-    sendURL(URLinput.value, ext.value)
+    convertBtn.style.opacity = "0.5"
+    convertBtn.style.pointerEvents = "none"
+
+    if (URLinput.value.includes("youtu") && URLinput.value.includes("://") && URLinput.value.includes("watch")) {
+        sendURL(URLinput.value, ext.value)
+    } else {
+        URLinput.placeholder = "No URL provided!"
+        setTimeout(() => {
+            convertBtn.style.opacity = "unset"
+            convertBtn.style.pointerEvents = "unset"
+            URLinput.placeholder = "Paste a YouTube URL here"
+        }, 2000)
+    }
 })
 
 function sendURL(URL, ext) {
     window.location.href = `https://savetube.herokuapp.com/download?URL=${URL}&ext=${ext}`
+    setTimeout(() => {
+        convertBtn.style.opacity = "unset"
+        convertBtn.style.pointerEvents = "unset"
+    }, 10000)
 }
