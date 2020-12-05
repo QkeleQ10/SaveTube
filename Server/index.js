291240c8a91e8cb6e005d187d6ff775a4ee37b98
Quinten Althues <quintenra@gmail.com>
2020-12-05 14:32:18 +0100

    combine video and audio

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const ytdl = require('ytdl-core')
+const ffmpeg = require('ffmpeg')
 const app = express()
 
 app.use(cors())
@@ -13,18 +14,43 @@ app.get("/download", async (req, res) => {
     try {
         var URL = req.query.URL
         var ext = req.query.ext
-        var filter
-        var quality
-        if (ext === "mp3") {
-            filter = "audioonly"
-            quality = "highestaudio"
-        } else {
-            filter = "videoandaudio"
-            quality = "highestvideo"
-        }
 
         res.header("Content-Disposition", `attachment; filename="download.${ext}"`)
-        ytdl(URL, { format: ext, filter: filter, quality: quality }).pipe(res)
+        if (ext === "mp3") {
+            ytdl(URL, { format: ext, filter: "audioonly", quality: "highestaudio" }).pipe(res)
+        } else {
+            const video = ytdl(url, { filter: 'videoonly' });
+            const audio = ytdl(url, { filter: 'audioonly', highWaterMark: 1 << 25 });
+            // Start the ffmpeg child process
+            const ffmpegProcess = cp.spawn(ffmpeg, [
+                // Remove ffmpeg's console spamming
+                '-loglevel', '0', '-hide_banner',
+                '-i', 'pipe:4',
+                '-i', 'pipe:5',
+                '-reconnect', '1',
+                '-reconnect_streamed', '1',
+                '-reconnect_delay_max', '4',
+                // Rescale the video
+                '-vf', 'scale=1980:1080',
+                // Choose some fancy codes
+                '-c:v', 'libx265', '-x265-params', 'log-level=0',
+                '-c:a', 'flac',
+                // Define output container
+                '-f', 'matroska', 'pipe:6',
+            ], {
+                windowsHide: true,
+                stdio: [
+                    /* Standard: stdin, stdout, stderr */
+                    'inherit', 'inherit', 'inherit',
+                    /* Custom: pipe:4, pipe:5, pipe:6 */
+                    'pipe', 'pipe', 'pipe',
+                ],
+            });
+
+            audio.pipe(ffmpegProcess.stdio[4])
+            video.pipe(ffmpegProcess.stdio[5])
+            ffmpegProcess.stdio[6].pipe(res) // Combining and piping the streams for download directly to the response
+        }
     } catch (err) { throw new Error('BROKEN') }
 })
 
