892a45886f7f021209ed70142e925f934e235379
Quinten Althues <quintenra@gmail.com>
2020-12-05 14:14:03 +0100

    Better quality?

--- Server/index.js
+++ Server/index.js
@@ -14,10 +14,17 @@ app.get("/download", async (req, res) => {
         var URL = req.query.URL
         var ext = req.query.ext
         var filter
-        if (ext === "mp3") filter = "audioonly"
-        else filter = "videoandaudio"
+        var quality
+        if (ext === "mp3") {
+            filter = "audioonly"
+            quality = "highestaudio"
+        } else {
+            filter = "videoandaudio"
+            quality = "highest"
+        }
+
         res.header("Content-Disposition", `attachment; filename="download.${ext}"`)
-        ytdl(URL, { format: ext, filter: filter, }).pipe(res)
+        ytdl(URL, { format: ext, filter: filter, quality: quality }).pipe(res)
     } catch (err) { throw new Error('BROKEN') }
 })
 
